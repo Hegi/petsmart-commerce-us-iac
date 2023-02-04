9cb2955de800e0fb5f6cc88f964ec46df050c8df
Istvan Hegedus <istvan.hegedus@valtech.com>
2023-02-03 22:03:15 -0700

    Add url

--- main.tf
+++ main.tf
@@ -7,6 +7,38 @@ terraform {
   }
 }
 
+variable "ct_client_id" {
+    description = "Commercetools provider config for ct_client_id."
+    type = string
+    default = ""
+}
+variable "ct_client_secret" {
+    description = "Commercetools provider config for ct_client_secret."
+    type = string
+    default = ""
+}
+variable "ct_project_key" {
+    description = "Commercetools provider config for ct_project_key."
+    type = string
+    default = ""
+}
+variable "ct_scopes" {
+    description = "Commercetools provider config for ct_scopes."
+    type = string
+    default = ""
+}
+variable "ct_api_url" {
+    description = "Commercetools provider config for ct_api_url."
+    type = string
+    default = "https://api.us-central1.gcp.commercetools.com"
+}
+variable "ct_token_url" {
+    description = "Commercetools provider config for ct_token_url."
+    type = string
+    default = "https://auth.us-central1.gcp.commercetools.com"
+}
+
+
 
 provider "commercetools" {
   client_id     = var.ct_client_id
